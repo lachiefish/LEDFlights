0531a61b3b32b7713faf7af40d122d944109e27e
Lachlan Fisher <lachiefish@gmail.com>
2025-07-17 16:58:31 +1000

    Added serial log for setting local time

--- src/time_manager.cpp
+++ src/time_manager.cpp
@@ -5,6 +5,7 @@
 
 void TimeManager::begin(float utc_offset, float dst_offset, std::string ntp1, std::string ntp2)
 {
+  Serial.println("[TIME] Setting local time");
   const std::string tz = "AEST-10AEDT-11,M10.1.0/2,M4.1.0/3"; // Sydney timezone with DST rules
   configTzTime(tz.c_str(), ntp1.c_str(), ntp2.c_str());
   while (time(nullptr) < 100000)
